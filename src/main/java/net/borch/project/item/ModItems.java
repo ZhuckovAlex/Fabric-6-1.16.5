260e6e136c0296bef171b231f03d04272c1d6a45
ZhuckovAlex <91392232+ZhuckovAlex@users.noreply.github.com>
2022-03-13 20:41:20 +0300

    Fabric-6-1.16.5

---
+++ src/main/java/net/borch/project/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.borch.project.item;
+
+import net.borch.project.ProjectMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems {
+
+    public static final Item SUN = registerItem("sun",
+            new Item(new FabricItemSettings().group(ModItemGroup.SUN)));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(ProjectMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        System.out.println("Registering Mod Items for " + ProjectMod.MOD_ID);
+    }
+}
